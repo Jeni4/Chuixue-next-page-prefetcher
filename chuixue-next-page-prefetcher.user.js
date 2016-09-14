755bd0e184435a776ab756fcd5b0ae09d5fd982c
Christoffer <Jeni4@users.noreply.github.com>
2016-09-15 00:32:48 +0200

    Update chuixue-next-page-prefetcher.user.js

--- chuixue-next-page-prefetcher.user.js
+++ chuixue-next-page-prefetcher.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Chuixue next-page prefetcher
 // @namespace    https://github.com/Jeni4/Chuixue-next-page-prefetcher/
-// @version      1.0.1.5
+// @version      1.0.1.6
 // @description  Prefetches the next page when viewing a manhua chapter on chuixue.com so the surfing experience is percived much more smoothly.
 // @author       Jeni4
 // @match        http://www.chuixue.com/manhua/*/*.html*
